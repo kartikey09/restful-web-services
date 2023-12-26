fae271ced7c7686ed684b6ce0ec539afe7ada8f5
karikey rana <rana.kartikey999@gmail.com>
2023-12-26 14:16:08 +0530

    L7

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	private static List<User> users = new ArrayList<>(); 
+	static {
+		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(2, "Eve", LocalDate.now().minusYears(35)));
+		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+}

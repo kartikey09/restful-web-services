e5f165e5a90b077841580ab95a5404bd7abb40fc
karikey rana <rana.kartikey999@gmail.com>
2023-12-26 12:51:24 +0530

    L5

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloWorld/helloWorld.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloWorld/helloWorld.java
@@ -1,9 +1,11 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloWorld;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//RestController by default has the @ResponseBody annotation how ever with @Controller we will have to attach @ResponseBody else a string returning func will return a view 
+@RestController
 public class helloWorld {
 	@GetMapping("hello-world")
 	public String helloWorldfunc() {
@@ -13,5 +15,11 @@ public class helloWorld {
 	@GetMapping("hello-world-bean")
 	public helloWorldBean helloWorldPOJO() {
 		return new helloWorldBean("Hello World Bean");
+	}
+	
+	//Path variables and reqParams are different !
+	@GetMapping("hello-world-bean/path-variable/{name}")
+	public helloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new helloWorldBean(String.format("Hello world, %s", name));
 	}
 }

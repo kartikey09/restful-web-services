5db236c2352c335c4137b7e08fb1012da9de150f
karikey rana <rana.kartikey999@gmail.com>
2023-12-29 07:25:25 +0530

    L18

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloWorld/helloWorld.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloWorld/helloWorld.java
@@ -1,5 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloWorld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 //RestController by default has the @ResponseBody annotation how ever with @Controller we will have to attach @ResponseBody else a string returning func will return a view 
 @RestController
 public class helloWorld {
+	
+	private MessageSource messageSource;
+	
+	@Autowired
+	public helloWorld(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
 	@GetMapping("hello-world")
 	public String helloWorldfunc() {
 		return "Hello World";
+	}
+	
+	@GetMapping("hello-world-i18n")
+	public String helloWorldfunci18n() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default message", locale );
 	}
 	
 	@GetMapping("hello-world-bean")

23af2b2b4a3d1e564a036144bfdd5e70feabcda4
karikey rana <rana.kartikey999@gmail.com>
2023-12-30 21:06:52 +0530

    L28

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -4,18 +4,24 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 2, message = "name should have atleat 2 chars")// validation
-	@JsonProperty("user_name")	// to give name to this field when sending in json format
+//	@JsonProperty("user_name")	  to give name to this field when sending in json format
 	private String name;
 	
 	@Past(message = "birth date should be in the past")// validation
-	@JsonProperty("birth_date")
+//	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details(id, birth_date, name) values (10001, current_date(), 'Rana')
+insert into user_details(id, birth_date, name) values (10002, current_date(), 'Hari')
+insert into user_details(id, birth_date, name) values (10003, current_date(), 'Satish')
